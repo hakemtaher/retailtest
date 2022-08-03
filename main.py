ac7ab464fd969ea41caf216038ff8aadaa07420c
hakemmetawea <hakem.taher@yahoo.com>
2022-08-03 06:29:57 +0300

    update business settings

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(120)
 load_dotenv()
 
-login.loginfo(driver, 'hakem', '123456')
+# login.loginfo(driver, 'hakem', '123456')
 
-business.addBusiness(driver, 'bus.json')
+# business.addBusiness(driver, 'bus.json')
 
-login.logout(driver)
+# login.logout(driver)
 
 f = open("data.json", "r")
 file_data = json.loads(f.read())
@@ -32,19 +32,29 @@ f.close()
 last_element = file_data["users_detail"].pop()
 login.loginfo(driver, last_element['username'], '123456')
 
-driver.get(os.environ["APP_URL"] + "tax_rates/create")
+driver.get(os.environ["APP_URL"] + "tax-rates/create")
 driver.find_element(By.XPATH, '//*[@id="name"]').send_keys('VAT')
 driver.find_element(By.XPATH, '//*[@id="amount"]').send_keys('10')
 driver.find_element(By.XPATH, '//*[@id="tax_rate_add_form"]/div[3]/button[1]').click()
 
+vat_data = json.loads(driver.find_element(By.XPATH, '/html/body/pre').text)
+vat_id = vat_data["data"]["id"]
+
 driver.get(os.environ["APP_URL"] + "business/settings")
+Select(driver.find_element(By.XPATH, '//*[@id="accounting_method"]')).select_by_value('avco')
 Select(driver.find_element(By.XPATH, '//*[@id="bussiness_edit_form"]/div[1]/div/div/div[2]/div[1]/div[4]/div/div/div/select')).select_by_value('3')
-driver.find_element(By.XPATH, '')
-driver.find_element(By.XPATH, '//*[@id="name"]').send_keys('VAT')
-driver.find_element(By.XPATH, '//*[@id="amount"]').send_keys('10')
+
+driver.find_element(By.XPATH, '//*[@id="bussiness_edit_form"]/div[1]/div/div/div[1]/div/a[2]').click()
+driver.find_element(By.XPATH, '//*[@id="bussiness_edit_form"]/div[1]/div/div/div[2]/div[2]/div/div[6]/div/div/label/div').click()
+
+
 driver.find_element(By.XPATH, '//*[@id="bussiness_edit_form"]/div[2]/div/button').click()
 
-
+WebDriverWait(driver, 120).until(
+    EC.presence_of_element_located(
+        (By.ID, 'hakem')
+    )
+)
 driver.get("https://svretailmodule.smartqr.app/products/create")
 driver.find_element(By.XPATH, '//*[@id="name"]').send_keys(unique.unique('product '))
 driver.find_element(By.XPATH, '//*[@id="single_dpp"]').send_keys('10')
@@ -54,9 +64,3 @@ driver.find_element(By.XPATH, '//*[@id="single_dpp"]').send_keys('10')
 # driver.find_element(By.XPATH, '').send_keys()
 # driver.find_element(By.XPATH, '').send_keys()
 # driver.find_element(By.XPATH, '').send_keys()
-
-WebDriverWait(driver, 120).until(
-    EC.presence_of_element_located(
-        (By.ID, 'hakem')
-    )
-)
